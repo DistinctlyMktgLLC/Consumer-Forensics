4aec8e3df1403a8dc97e41af0027ccea74c5992a
Distinct.ly Mktg, Inc. <55642043+DistinctlyMktgLLC@users.noreply.github.com>
2024-07-31 03:16:29 +0000

    repo update

--- Pages/people_findr.py
+++ Pages/people_findr.py
@@ -17,9 +17,6 @@ def app():
     # Standardize column names: lowercase and replace spaces with underscores
     data_sampled.columns = [col.lower().replace(' ', '_') for col in data_sampled.columns]
 
-    # Debugging: Print the columns to check
-    st.write(data_sampled.columns.tolist())
-
     st.sidebar.header("I want to understand People")
     
     kind_of_person = st.sidebar.multiselect("Kind of Person", data_sampled['kind_of_person'].dropna().unique(), help="Who they are - Kind of Person")
@@ -28,14 +25,14 @@ def app():
     how_to_connect = st.sidebar.multiselect("How to Connect", data_sampled['how_to_connect'].dropna().unique(), help="What's their value - How to Connect")
     what_theyre_into = st.sidebar.multiselect("What they’re into", data_sampled['what_theyre_into'].dropna().unique(), help="What's their value - What they’re into")
     how_they_feel = st.sidebar.multiselect("How They Feel", data_sampled['how_they_feel'].dropna().unique(), help="What's their value - How They Feel")
-    how_you_hook_them = st.sidebar.multiselect("How you hook them", data_sampled['how_you_hook_them'].dropna().unique(), help="What's their value - How you hook them")
+    how_you_hook_them = st.sidebar.multiselect("How you hook them", data_sampled['how_you_hook_them_'].dropna().unique(), help="What's their value - How you hook them")
 
     sign = st.sidebar.multiselect("Sign", data_sampled['sign'].dropna().unique(), help="Demographic - Sign")
     income = st.sidebar.multiselect("Income", data_sampled['income'].dropna().unique(), help="Demographic - Income")
     race = st.sidebar.multiselect("Race", data_sampled['race'].dropna().unique(), help="Demographic - Race")
-    marstat = st.sidebar.multiselect("Marital Status", data_sampled['marstat'].dropna().unique(), help="Demographic - Marital Status")
-    educ = st.sidebar.multiselect("Education", data_sampled['educ'].dropna().unique(), help="Demographic - Education")
-    employ = st.sidebar.multiselect("Employment", data_sampled['employ'].dropna().unique(), help="Demographic - Employment")
+    marstat = st.sidebar.multiselect("Marital Status", data_sampled['status'].dropna().unique(), help="Demographic - Marital Status")
+    educ = st.sidebar.multiselect("Education", data_sampled['education'].dropna().unique(), help="Demographic - Education")
+    employ = st.sidebar.multiselect("Employment", data_sampled['employment'].dropna().unique(), help="Demographic - Employment")
     gender = st.sidebar.multiselect("Gender", data_sampled['gender'].dropna().unique(), help="Demographic - Gender")
 
     filtered_data = data_sampled.copy()
@@ -53,7 +50,7 @@ def app():
     if how_they_feel:
         filtered_data = filtered_data[filtered_data['how_they_feel'].isin(how_they_feel)]
     if how_you_hook_them:
-        filtered_data = filtered_data[filtered_data['how_you_hook_them'].isin(how_you_hook_them)]
+        filtered_data = filtered_data[filtered_data['how_you_hook_them_'].isin(how_you_hook_them)]
     if sign:
         filtered_data = filtered_data[filtered_data['sign'].isin(sign)]
     if income:
@@ -61,11 +58,11 @@ def app():
     if race:
         filtered_data = filtered_data[filtered_data['race'].isin(race)]
     if marstat:
-        filtered_data = filtered_data[filtered_data['marstat'].isin(marstat)]
+        filtered_data = filtered_data[filtered_data['status'].isin(marstat)]
     if educ:
-        filtered_data = filtered_data[filtered_data['educ'].isin(educ)]
+        filtered_data = filtered_data[filtered_data['education'].isin(educ)]
     if employ:
-        filtered_data = filtered_data[filtered_data['employ'].isin(employ)]
+        filtered_data = filtered_data[filtered_data['employment'].isin(employ)]
     if gender:
         filtered_data = filtered_data[filtered_data['gender'].isin(gender)]
     
@@ -77,9 +74,9 @@ def app():
         "html": "<b>Sign:</b> {sign}<br/>"
                 "<b>Income:</b> {income}<br/>"
                 "<b>Race:</b> {race}<br/>"
-                "<b>Marital Status:</b> {marstat}<br/>"
-                "<b>Education:</b> {educ}<br/>"
-                "<b>Employment:</b> {employ}<br/>"
+                "<b>Marital Status:</b> {status}<br/>"
+                "<b>Education:</b> {education}<br/>"
+                "<b>Employment:</b> {employment}<br/>"
                 "<b>Gender:</b> {gender}<br/>"
                 "<b>Kind of Person:</b> {kind_of_person}<br/>"
                 "<b>True Colors:</b> {true_colors}<br/>"
@@ -87,7 +84,7 @@ def app():
                 "<b>How to Connect:</b> {how_to_connect}<br/>"
                 "<b>What they’re into:</b> {what_theyre_into}<br/>"
                 "<b>How They Feel:</b> {how_they_feel}<br/>"
-                "<b>How you hook them:</b> {how_you_hook_them}<br/>",
+                "<b>How you hook them:</b> {how_you_hook_them_}<br/>",
         "style": {
             "backgroundColor": "steelblue",
             "color": "white"
